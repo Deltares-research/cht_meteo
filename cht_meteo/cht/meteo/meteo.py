ebc118e5e33bb9d9019201611c89c4feb5f68120
Maarten van Ormondt <maarten.vanormondt@deltares.nl>
2024-09-09 11:31:14 +0200

    added interpolation option in 'old' cht meteo for interpolating to spw

--- cht_meteo/cht/meteo/meteo.py
+++ cht_meteo/cht/meteo/meteo.py
@@ -485,6 +485,50 @@ class MeteoGrid():
         meteo_source.set_index('date', inplace=True)
         self.meteo_source = meteo_source        
 
+    def interpolate(self, quantity, requested_time, lon, lat, no_data_value=0.0):
+        # Find index of quantity in self.variables list
+        if quantity == "wind_u" or quantity == "wind_x":
+            quantity = "wind"
+            ivec     = True
+            col      = 0
+        elif quantity == "wind_v" or quantity == "wind_y":
+            quantity = "wind"
+            ivec     = True
+            col      = 1
+        else:
+            ivec     = False
+
+        if quantity not in self.parameters:
+            # print("Quantity " + quantity + " not found in meteo dataset")
+            # V is empty array with zeros
+            v = np.zeros(np.shape(lat)) + no_data_value
+            return v
+
+        ind = self.parameters.index(quantity)
+        t   = self.time
+        # No interpolation for now
+        it = np.where(t>=requested_time)
+        # Check if it is not empty
+        if len(it[0]) == 0:
+            # V is empty array with zeros
+            v = np.zeros(np.shape(lat)) + no_data_value
+            return v
+        
+        it = it[0][0]
+        if ivec:
+            if col == 0:
+                v0 = self.quantity[ind].u[it,:,:]
+            else:
+                v0 = self.quantity[ind].v[it,:,:]
+        else:
+            v0 = self.quantity[ind].val[it,:,:]
+
+        # Make interpolator
+        interp = interpolate.RegularGridInterpolator((self.y, self.x), v0)
+
+        # Return numpy array with interpolated values
+        v = interp((lat, lon))
+        return v
 
     def read_from_delft3d(self, file_name, crs=None):
         pass
