86e8879f7090a5fa843ca03df42a437beecb18ba
vanasseltk <167875592+vanasseltk@users.noreply.github.com>
2026-04-13 14:17:55 +0200

    add example nb

--- cht_meteo/__init__.py
+++ cht_meteo/__init__.py
@@ -7,6 +7,7 @@ from cht_meteo.database import MeteoDatabase
 from cht_meteo.dataset import MeteoDataset
 from cht_meteo.gfs_anl_0p50 import MeteoDatasetGFSAnalysis0p50
 from cht_meteo.gfs_forecast_0p25 import MeteoDatasetGFSForecast0p25
+from cht_meteo.era5_reanalysis_0p25 import MeteoDatasetERA5Reanalysis0p25
 
 __all__ = ["MeteoDatabase"]
 
@@ -19,12 +20,8 @@ def dataset(name=None, source=None, path=None, **kwargs):
             md = MeteoDatasetGFSForecast0p25(name=name, path=path, **kwargs)
         elif source == "gfs_analysis_0p50":
             md = MeteoDatasetGFSAnalysis0p50(name=name, path=path, **kwargs)
-        else:
-            md = MeteoDataset(name=name)
-            print(
-                f"Error while reading meteo database : source {source} not recognized"
-            )
-
+        elif source == "era5_reanalysis_0p25":
+            md = MeteoDatasetERA5Reanalysis0p25(name=name, path=path, **kwargs)
     else:
         # Use generic meteo dataset (this does not have download functionality)
         md = MeteoDataset(name=name)
